0eaffd7abfb63438320830e680af6c707b213c85
Brutin Bjorn <bjorn.brutin@student.howest.be>
2021-06-03 22:17:00 +0200

    added game project and set up link for minigin.

--- Minigin/SoundSystem.h
+++ Minigin/SoundSystem.h
@@ -28,7 +28,9 @@ public:
 	SoundSystem() = default;
 	virtual ~SoundSystem() = default;
 	virtual void play( unsigned short id, float volume, const std::string& fileName) = 0;
-	virtual void loadSound(int id, const std::string source) = 0;
+	virtual void playEffect(unsigned short id, float volume) = 0;
+	virtual void loadSound(int id, const std::string& source) = 0;
+	virtual void LoadSoundEffect(int id, const std::string& source) = 0;
 };	
 
 
@@ -38,13 +40,9 @@ class SDLSoundSystem final : public SoundSystem
 public:
 
 	
-	//void PlaySound(int id, int volume) override
-	//{
-	//	Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096);
-	//	int channel = Mix_PlayMusic(m_Songs[id],-1);
-	//	Mix_Volume(channel, volume);
-	//}
-
+	// mix_Halt stop and reset
+	// mixpause stop to current time of song
+	
 	
 	SDLSoundSystem() : SoundSystem()
 	{
@@ -61,19 +59,29 @@ public:
 	{
 		m_SoundQueue.clear();
 
+
+		for (std::map<int,Mix_Music*>::iterator songitr = m_Songs.begin(); songitr != m_Songs.end(); songitr++)
+		{
+			Mix_FreeMusic(songitr->second);
+		}
+
+
+		for (std::map<int, Mix_Chunk*>::iterator effectitr = m_soundEffects.begin(); effectitr != m_soundEffects.end(); effectitr++)
+		{
+			Mix_FreeChunk(effectitr->second);
+		}
+		
+
+		
 		Mix_CloseAudio();
 		Mix_Quit();
 
 		m_stopping = true;
 		m_thread.join();
 
-
-
-
-
 	}
 	
-	void loadSound(int id,const std::string source) override
+	void loadSound(int id,const std::string& source) override
 	{
 
 		std::string fullpath = "../Data/music/";
@@ -89,7 +97,22 @@ public:
 		else
 		std::cout << "no music found at: " << fullpath << " error: " << Mix_GetError << std::endl;
 	}
-	
+
+	void LoadSoundEffect(int id, const std::string& source) override
+	{
+		std::string fullpath = "../Data/music/";
+		fullpath.append(source);
+
+		Mix_Chunk* effect = Mix_LoadWAV(source.c_str());
+		if (effect != nullptr)
+		{
+			m_soundEffects[id] = effect;
+			std::cout << "effect loaded at: " << id << std::endl;
+		}
+		else
+			std::cout << "no effect found at: " << fullpath << " error: " << Mix_GetError << std::endl;
+
+	}
 	
 	void play(unsigned short id, float volume, const std::string& fileName) override
 	{
@@ -101,12 +124,18 @@ public:
 		m_WorkAvailable.notify_one();
 	};
 
+	void playEffect(unsigned short id, float volume) override
+	{
+		PlaySoundEffect(id, volume);
+	}
+	
 	void Update()
 	{
 		do
 		{
 			std::unique_lock<std::mutex> mlock{ m_Mutex };
-			if (!m_SoundQueue.empty() && 0 == Mix_PlayingMusic())
+
+			if ( !m_SoundQueue.empty() && 0 == Mix_PlayingMusic()) // check if there are songs to play AND is the current music not playing?
 			{
 				PlaySound soundToPlay = m_SoundQueue.front();
 				m_SoundQueue.pop_front();
@@ -122,7 +151,7 @@ public:
 
 		} while (!m_stopping);
 		
-		std::cout << "update ended" << std::endl;
+		std::cout << "update sound ended" << std::endl;
 	}
 
 private:
@@ -135,6 +164,16 @@ private:
 
 	};
 
+	void PlaySoundEffect(int effectId, float volume)
+	{
+		float clampvolume = std::clamp(volume, 0.f, 1.f);
+
+		Mix_VolumeChunk(m_soundEffects[effectId], static_cast<int>(clampvolume * MIX_MAX_VOLUME));
+		Mix_PlayChannel(-1, m_soundEffects[effectId],0);
+
+		
+		
+	}
 	//std::map<int, Mix_Chunk*> m_effects;
 
 	//squeue
@@ -142,8 +181,10 @@ private:
 	std::list<PlaySound> m_SoundQueue;
 
 	std::map<int, Mix_Music*> m_Songs;
+	std::map<int, Mix_Chunk*> m_soundEffects;
 
 	std::atomic_bool m_isMuted = false;
+	std::atomic_bool m_playNextSong = false;
 	std::atomic_bool m_stopping = false;
 	//thread
 	std::thread m_thread;
@@ -155,7 +196,9 @@ private:
 class NullSoundSystem final : public SoundSystem
 {
 	void play( unsigned short, float , const std::string&) override {};
-	void loadSound(int , const std::string ) override {};
+	void loadSound(int, const std::string& ) override {};
+	void LoadSoundEffect(int, const std::string& ) override {};
+	void playEffect(unsigned short, float ) override {};
 };
 
 
